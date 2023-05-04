625293847276fa103be7b94cf69abe9f39690a2c
ameen <ameen55668@gmail.com>
2023-05-04 10:02:49 +0500

    add crud

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require("express");
 const bodyParser = require('body-parser')
 
 const app = express()
+
+//Use External Lib. to Encrypt the data
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+//port Number
 const port = 800
-const data = []
 
+//Main Array
+let data = []
+
+//Use Post Request Method
 app.get('/', (req, res) => {
     res.status(200).json(data);
 })
 
-
+//Use Params
 app.get('/get/:id', (req, res) => {
-    const paramData = []
+    let paramData = []
     data.forEach((obj) => {
         if (obj.id === Number(req.params.id)) {
             paramData.push(obj)
@@ -22,8 +28,10 @@ app.get('/get/:id', (req, res) => {
     })
     res.status(200).json(paramData);
 })
+
+//Use Query
 app.get('/get', (req, res) => {
-    const QData = []
+    let QData = []
     data.forEach((obj) => {
         if (obj.name === req.query.name) {
             QData.push(obj)
@@ -32,6 +40,7 @@ app.get('/get', (req, res) => {
     res.status(200).json(QData);
 })
 
+//Use Post Request Method
 app.post('/create', (req, res) => {
     data.push({
         ...req.body,
@@ -41,6 +50,33 @@ app.post('/create', (req, res) => {
     res.status(200).json(data);
 })
 
+//Update the Data
+app.post('/update/:id/', (req, res) => {
+    let updatedData = []
+    data.forEach((obj) => {
+        if (obj.id === Number(req.params.id)) {
+            obj.name = req.query.name
+        }
+        updatedData.push(obj)
+    })
+    data = updatedData
+    res.status(200).json(data)
+})
+
+//Delete the Data
+app.post('/delete/:id/', (req, res) => {
+    let updatedData = []
+    data.forEach((obj) => {
+        if (obj.id !== Number(req.params.id)) {
+            updatedData.push(obj)
+        } 
+    })
+    data = updatedData
+    res.status(200).json(data)
+})
+
+
+//Run The Server
 app.listen(port, () => {
     console.log(`server running on port ${port} sucessfully...`);
 })
